c058c175bc3d2fcc55f40b9dd0dda4bac3859f7b
Md Saney Alam <codecamper@outlook.com>
2018-10-16 02:24:04 -0400

    User Login created

--- app/models.py
+++ app/models.py
@@ -2,8 +2,11 @@ from app import db
 from datetime import datetime
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__= 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(256), nullable=False)
@@ -13,8 +16,13 @@ class User(db.Model):
     password_hash = Column(String(128))
 
     def __repr__(self):
-        return '<User {}>'.format(self.name)    
+        return '<User {}>'.format(self.name)   
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password) 
 
 
 class Category(db.Model):
@@ -29,7 +37,6 @@ class Category(db.Model):
 
 
 
-
 class Item(db.Model):
     __tablename__= 'item'
     id = Column(Integer, primary_key=True)
@@ -40,3 +47,10 @@ class Item(db.Model):
 
     def __repr__(self):
         return '<Item {}>'.format(self.title)
+
+
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,13 @@
-from app import app
-from flask import render_template, flash
-from app.forms import LoginForm
+from app import app, db
+from flask import render_template, flash, url_for, redirect, request
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Saney'}
     catalogs = [
@@ -20,10 +24,42 @@ def index():
 
 ## =============================  Login Management System ==================================
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(name=form.name.data, username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for User: {}'.format(form.username.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid Username or Password.')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
